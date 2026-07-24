d2accb943ffd157839906cefa628cbb2499eca64
quvvxx <quvvxx2@gmail.com>
2026-07-24 15:19:21 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/dms/devrytime/DevrytimeBeApplication.java
@@ -0,0 +1,13 @@
+package com.dms.devrytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevrytimeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevrytimeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dms/devrytime/DevrytimeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.devrytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevrytimeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
