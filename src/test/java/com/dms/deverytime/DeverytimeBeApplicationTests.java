3d5cb2493e52d4ccdd67395a625e7d7c9b26481f
quvvxx <quvvxx2@gmail.com>
2026-08-21 16:04:14 +0900

    refactor: (#15) 테스트 클래스명 표기 수정

--- src/test/java/com/dms/deverytime/DeverytimeBeApplicationTests.java
+++ src/test/java/com/dms/deverytime/DeverytimeBeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DeverytimeBeApplicationTests {
+class DeveryTimeBeApplicationTests {
 
     @Test
     void contextLoads() {
