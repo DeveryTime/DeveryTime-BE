e479c8df7663a4ed1eae6068a9572cd740753cee
quvvxx <quvvxx2@gmail.com>
2026-08-21 15:56:49 +0900

    refactor: (#15) 프로젝트명 및 패키지명 오타 수정

--- src/main/java/com/dms/devrytime/domain/auth/presentation/dto/request/LogoutRequest.java
+++
@@ -1,9 +0,0 @@
-package com.dms.devrytime.domain.auth.presentation.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LogoutRequest(
-        @NotBlank(message = "리프레시 토큰을 전달해주세요.")
-        String refreshToken
-) {
-}
