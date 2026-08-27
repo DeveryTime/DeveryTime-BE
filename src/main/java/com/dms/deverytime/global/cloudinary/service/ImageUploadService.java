dc2c0377c85b8bb30ba3d48dfd415e5ef012cd95
quvvxx <quvvxx2@gmail.com>
2026-08-27 15:12:01 +0900

    feat: (#21) Apache Tika 기반 이미지 파일 검증 추가

--- src/main/java/com/dms/deverytime/global/cloudinary/service/ImageUploadService.java
+++ src/main/java/com/dms/deverytime/global/cloudinary/service/ImageUploadService.java
@@ -6,6 +6,7 @@ import com.dms.deverytime.global.cloudinary.dto.ImageUploadResult;
 import com.dms.deverytime.global.exception.DeveryTimeException;
 import com.dms.deverytime.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.apache.tika.Tika;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
@@ -16,6 +17,7 @@ import java.util.Map;
 public class ImageUploadService {
 
     private final Cloudinary cloudinary;
+    private final Tika tika = new Tika();
     private static final long MAX_IMAGE_SIZE = 4 * 1024 * 1024;
 
     public ImageUploadResult upload(MultipartFile file, String folder){
@@ -47,10 +49,15 @@ public class ImageUploadService {
         if (file.getSize() > MAX_IMAGE_SIZE)
             throw new DeveryTimeException(ErrorCode.IMAGE_FILE_TOO_LARGE);
 
-        String contentType = file.getContentType();
+        try {
+            String detectedType = tika.detect(file.getInputStream());
 
-        if (contentType == null || !(contentType.equals("image/jpeg")
-                || contentType.equals("image/png") || (contentType.equals("image/webp"))))
+            if (!(detectedType.equals("image/jpeg") || detectedType.equals("image/png")
+                    || detectedType.equals("image/webp")))
+                throw new DeveryTimeException(ErrorCode.INVALID_IMAGE_FILE);
+
+        } catch (IOException e) {
             throw new DeveryTimeException(ErrorCode.INVALID_IMAGE_TYPE);
+        }
     }
 }
