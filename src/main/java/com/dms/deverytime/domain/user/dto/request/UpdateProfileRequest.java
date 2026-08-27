b05cabf0df69fdc2052fa136a28e19a7d5129dbd
quvvxx <quvvxx2@gmail.com>
2026-08-27 16:54:21 +0900

    fix: (#18) 빈 사용자명 입력 검증 추가

--- src/main/java/com/dms/deverytime/domain/user/dto/request/UpdateProfileRequest.java
+++ src/main/java/com/dms/deverytime/domain/user/dto/request/UpdateProfileRequest.java
@@ -1,9 +1,11 @@
 package com.dms.deverytime.domain.user.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UpdateProfileRequest(
         @Size(max = 10, message = "아이디는 10자 이하로 입력해주세요.")
+        @Pattern(regexp = ".*\\S.*", message = "아이디는 공백일 수 없습니다.")
         String username,
         boolean deleteProfileImage
 ) {
