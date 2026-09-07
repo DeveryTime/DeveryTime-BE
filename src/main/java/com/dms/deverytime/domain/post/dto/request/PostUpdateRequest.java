f7b49e911035b89b7370ac6727d07f14edbe89e4
ch0ijimin323 <jiminchoi7127@gmail.com>
2026-09-07 19:28:44 +0900

    fix: (#20) PostUpdateRequest에 유효성 검증 어노테이션 추가

--- src/main/java/com/dms/deverytime/domain/post/dto/request/PostUpdateRequest.java
+++ src/main/java/com/dms/deverytime/domain/post/dto/request/PostUpdateRequest.java
@@ -1,11 +1,17 @@
 package com.dms.deverytime.domain.post.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class PostUpdateRequest {
 
+    @NotBlank
+    @Size (max = 255)
     private String title;
+
+    @NotBlank
     private String content;
 
     public PostUpdateRequest() {
