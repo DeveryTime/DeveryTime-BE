04cb5f0b237f06093b503bd3db84985676a97eff
ch0ijimin323 <jiminchoi7127@gmail.com>
2026-09-07 19:28:19 +0900

    fix: (#20) PostViewLogRepository에 deleteByPostId 추가

--- src/main/java/com/dms/deverytime/domain/post/repository/PostViewLogRepository.java
+++ src/main/java/com/dms/deverytime/domain/post/repository/PostViewLogRepository.java
@@ -4,7 +4,6 @@ import com.dms.deverytime.domain.post.entity.PostViewLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostViewLogRepository extends JpaRepository<PostViewLog, Long> {
-
     boolean existsByPostIdAndUserId(Long postId, Long userId);
-    //누군가 그걸 봤는지 안봤는지 질문
+    void deleteByPostId(Long postId);
 }
