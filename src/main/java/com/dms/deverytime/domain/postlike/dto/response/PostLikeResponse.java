b756b125bb5e436b898d9a5c040009a741cb08a7
Park junseong <20261306@dsm.hs.kr>
2026-09-08 10:01:45 +0900

    feat: (#29) 게시글 좋아요 응답 DTO 구현

--- src/main/java/com/dms/deverytime/domain/postlike/dto/response/PostLikeResponse.java
+++ src/main/java/com/dms/deverytime/domain/postlike/dto/response/PostLikeResponse.java
@@ -1,4 +1,16 @@
 package com.dms.deverytime.domain.postlike.dto.response;
 
+import lombok.Getter;
+
+@Getter
 public class PostLikeResponse {
+    private Long postId;
+    private Long userId;
+    private boolean liked;
+
+    public PostLikeResponse(Long postId, Long userId, boolean liked) {
+        this.postId = postId;
+        this.userId = userId;
+        this.liked = liked;
+    }
 }
