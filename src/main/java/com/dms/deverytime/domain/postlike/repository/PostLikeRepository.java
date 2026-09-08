1eec16674d9ed1840301827f4f5e44fa8511d293
Park junseong <20261306@dsm.hs.kr>
2026-09-08 09:31:54 +0900

    feat: (#29) 게시글 좋아요 저장소 구현

--- src/main/java/com/dms/deverytime/domain/postlike/repository/PostLikeRepository.java
+++ src/main/java/com/dms/deverytime/domain/postlike/repository/PostLikeRepository.java
@@ -1,15 +1,15 @@
-package com.dms.deverytime.domain.postlike.repository;
+    package com.dms.deverytime.domain.postlike.repository;
 
-import com.dms.deverytime.domain.postlike.entity.PostLike;
-import org.springframework.data.jpa.repository.JpaRepository;
+    import com.dms.deverytime.domain.postlike.entity.PostLike;
+    import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+    import java.util.Optional;
 
-public interface PostLikeRepository extends JpaRepository< PostLike,Long> {
-    boolean existsByPostIdAndUserId(Long postId, Long userId);
+    public interface PostLikeRepository extends JpaRepository< PostLike, Long> {
+        boolean existsByPostIdAndUserId(Long postId, Long userId);
 
-    Optional<PostLike> findByPostIdAndUserId(Long postId, Long userId);
+        Optional<PostLike> findByPostIdAndUserId(Long postId, Long userId);
 
-    long countByPostId(Long postId);
+        long countByPostId(Long postId);
 
-}
+    }
