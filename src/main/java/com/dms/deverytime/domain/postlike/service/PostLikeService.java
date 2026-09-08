c1992276956e0b7777ac4a8ae8338bcc171c06f9
Park junseong <20261306@dsm.hs.kr>
2026-09-08 18:27:09 +0900

    feat: (#29) 게시글 좋아요 등록 서비스 구현

---
+++ src/main/java/com/dms/deverytime/domain/postlike/service/PostLikeService.java
@@ -0,0 +1,53 @@
+package com.dms.deverytime.domain.postlike.service;
+
+
+import com.dms.deverytime.domain.post.entity.Post;
+import com.dms.deverytime.domain.post.repository.PostRepository;
+import com.dms.deverytime.domain.postlike.dto.response.PostLikeResponse;
+import com.dms.deverytime.domain.postlike.entity.PostLike;
+import com.dms.deverytime.domain.postlike.repository.PostLikeRepository;
+import com.dms.deverytime.domain.user.entity.User;
+import com.dms.deverytime.domain.user.repository.UserRepository;
+import com.dms.deverytime.global.exception.DeveryTimeException;
+import com.dms.deverytime.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostLikeService {
+    private final PostLikeRepository postLikeRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+
+    @Transactional
+    public PostLikeResponse createLike(Long postId, Long userId){
+        Post post = postRepository.findById(postId)
+                .orElseThrow(()->new DeveryTimeException(ErrorCode.POST_NOT_FOUND));
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new DeveryTimeException(ErrorCode.USER_NOT_FOUND));
+
+        boolean alreadyLiked =
+                postLikeRepository.existsByPostIdAndUserId(postId, userId);
+
+        if (alreadyLiked) {
+            throw new DeveryTimeException(ErrorCode.ALREADY_LIKED);
+        }
+
+        PostLike postLike = new PostLike(post,user);
+        postLikeRepository.save(postLike);
+
+        return new PostLikeResponse(
+          post.getId(),
+          user.getId(),
+                true
+
+        );
+
+    }
+
+
+}
